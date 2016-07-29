6e8aef2198d0ffd844ac96dfde5dae85da9a57c6
Piotr Goliasz <piotr.goliasz@post.pl>
2016-07-29 09:43:42 +0000

    Chat REST working

--- neuralconvo.lua
+++ neuralconvo.lua
@@ -4,7 +4,8 @@ require 'rnn'
 
 neuralconvo = {}
 
-torch.include('neuralconvo', 'cornell_movie_dialogs.lua')
+-- torch.include('neuralconvo', 'cornell_movie_dialogs.lua')
+torch.include('neuralconvo', 'cornell_movie_dialogs_doctor.lua')
 torch.include('neuralconvo', 'dataset.lua')
 torch.include('neuralconvo', 'seq2seq.lua')
 
